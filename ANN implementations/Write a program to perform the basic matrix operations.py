2a1fa453fd931a75295f32863edee256d398673f
Subhansh malviya <167747484+subh-775@users.noreply.github.com>
2024-11-28 11:24:59 +0530

    Create Write a program to perform the basic matrix operations.py

---
+++ ANN implementations/Write a program to perform the basic matrix operations.py
@@ -0,0 +1,13 @@
+# Write a program to perform the basic matrix operations
+
+import numpy as np
+
+# Matrices
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+# Operations
+print("Addition:\n", A + B)
+print("Subtraction:\n", A - B)
+print("Multiplication:\n", A * B)
+print("Division:\n", A / B)
